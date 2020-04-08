9dc16c97e2fdf02a6e436e90653e05f5187b34bd
menocu <jared.arave+git@gmail.com>
2020-04-07 18:05:58 -0600

    template commit

---
+++ __template/sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+  createCanvas(600,600,SVG)
+  // put setup code here
+}
+
+function draw() {
+  background(200);
+  fill(100,0,255);
+  stroke(1);
+  rect(100, 100, 50, 50);
+  rect(200, 100, 50, 50);
+  rect(200, 200, 50, 50);
+  // put drawing code here
+}
+
+function mouseClicked(){
+  
+}
